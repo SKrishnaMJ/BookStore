7c4ada750a4e75c6386a0e4ab03795d009d97188
SKrishnaMJ <146481712+SKrishnaMJ@users.noreply.github.com>
2024-11-24 11:57:53 -0600

    second commit

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.core.validators import MaxLengthValidator, MinLengthValidator
+from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
 class Book(models.Model):
@@ -7,6 +9,16 @@ class Book(models.Model):
     rating = models.IntegerField(validators=[MinLengthValidator(1), MaxLengthValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestSelling = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False, db_index=True)
+
+
+    def get_absolute_url(self):
+        return reverse("book-detail", args=[self.slug])
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+    
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Book
+from django.http import Http404
+from django.db.models import Avg
 
 # Create your views here.
+def index(request):
+    books = Book.objects.all().order_by("-rating")
+    books_count = books.count()
+    avg_rating = books.aggregate(Avg("rating"))
+    return render(request, "book_outlet/index.html", {
+        "books":books,
+        "books_count": books_count,
+        "avg_rating": avg_rating
+    })
+
+def book_detail(request, slug):
+    # try:
+    #     book = Book.objects.get(id=id)
+    # except:
+    #     raise Http404()
+    book = get_object_or_404(Book, slug=slug)
+    return render(request, "book_outlet/book_details.html", {
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestselling": book.is_bestSelling
+    })
